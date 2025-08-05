966ebbfd0f82fb23725e116e7805f5a4e1378caa
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-08-05 13:39:03 +0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: '/martians-login-form/',
+    plugins: [react()],
+});
