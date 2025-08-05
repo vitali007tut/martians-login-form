cfa7a0ee04c62828f4a51b08287eebb20f784a23
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-08-05 13:46:47 +0300

    fix path

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,14 @@
 import { LoginForm } from '../components/ui/LoginForm/LoginForm';
 import styles from './LoginPage.module.css';
+import image from '../assets/image.svg';
+import imageHover from '../assets/imageHover.svg';
 
 export const LoginPage = () => {
     return (
         <>
             <div className={styles.logoWrapper}>
-                <img src="src/assets/image-cropped.svg" alt="logo" className={styles.logoImage} />
-                <img src="src/assets/imageHover-cropped.svg" alt="logo" className={styles.logoImageHover} />
+                <img src={image} alt="image" className={styles.logoImage} />
+                <img src={imageHover} alt="image-hover" className={styles.logoImageHover} />
             </div>
             <main className={styles.pageContainer}>
                 <LoginForm />
