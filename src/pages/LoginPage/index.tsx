277acd970b9aceb445d85338a226cea08599a229
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-08-07 19:36:26 +0300

    add handleCopy

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,16 +1,53 @@
+import { useState } from 'react';
 import { LoginForm } from '../../components/ui/LoginForm';
 import styles from './styles.module.css';
 import image from '../../assets/image.svg';
-import imageHover from '../../assets/imageHover.svg'
+import imageHover from '../../assets/imageHover.svg';
 
 export const LoginPage = () => {
+    const [copiedEmail, setCopiedEmail] = useState(false);
+    const [copiedPassword, setCopiedPassword] = useState(false);
+
+    const handleCopy = (text: string, setCopied: React.Dispatch<React.SetStateAction<boolean>>) => {
+        navigator.clipboard.writeText(text).then(() => {
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        });
+    };
     return (
         <>
             <div className={styles.logoWrapper}>
                 <img src={image} alt="image" className={styles.logoImage} />
                 <img src={imageHover} alt="image-hover" className={styles.logoImageHover} />
             </div>
-            <div className={styles.infoText}>test@evilmartians.com | password: 12345678 </div>
+            <div className={styles.infoText}>
+                <span>Email:</span>
+                <span
+                    className={styles.copyableItem}
+                    onClick={() => handleCopy('test@evilmartians.com', setCopiedEmail)}
+                >
+                    test@evilmartians.com
+                    <span className={styles.copyIcon}>
+                        {!copiedEmail && <span className={styles.clipboardIcon}>📋</span>}
+                        {copiedEmail && <span className={styles.checkIcon}>✓</span>}
+                    </span>
+                </span>
+            </div>
+
+            <div className={styles.infoText}>
+                <span>Password:</span>
+                <span
+                    className={styles.copyableItem}
+                    onClick={() => handleCopy('12345678', setCopiedPassword)}
+                >
+                    12345678
+                    <span className={styles.copyIcon}>
+                        {!copiedPassword && <span className={styles.clipboardIcon}>📋</span>}
+                        {copiedPassword && <span className={styles.checkIcon}>✓</span>}
+                    </span>
+                </span>
+            </div>
+
             <main className={styles.pageContainer}>
                 <LoginForm />
             </main>
